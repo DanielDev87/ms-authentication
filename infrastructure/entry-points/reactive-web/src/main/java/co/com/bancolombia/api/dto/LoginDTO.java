138801e3ce6bd443806da2c22519b6460d277f3e
DanielDev87 <danielhymtj@icloud.com>
2025-08-31 18:51:54 -0500

    agregando el dto de login

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/LoginDTO.java
@@ -0,0 +1,15 @@
+package co.com.bancolombia.api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    @NotBlank(message = "El email no puede estar vacío.")
+    @Email(message = "Debe ser un formato de email válido.")
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacía.")
+    private String password;
+}
